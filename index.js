2e6c6500a901168a1d79a9dae9aded8d5d4c870d
Topottop <burhanovtimur047@gmail.com>
2024-06-27 17:01:03 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 function Delay(time){
    return new Promise(resolve => setTimeout(resolve, time));
 }
+function ResetActions(){
+   Actions = {
+  FightingNow: 0,
+  TravellingNow: 0,
+  TrainingNow: 0,
+  MeditatingNow: 0,
+   }
+}
 let display = document.getElementById("PowerGained");
 let Actions = {
   FightingNow: 0,
@@ -17,8 +25,8 @@ function Display(){
   display.innerHTML =`Level ${GameData.power.toFixed(2)}, Fight Efficiency ${GameData.powerPerSecond.toFixed(2)}`;
 }
 async function Fight() {
+  ResetActions()
   Actions.FightingNow=1;
-  Actions.TrainingNow=0;
   while (Actions.FightingNow==1){
     GameData.power += (GameData.powerPerSecond/10);
     Display();
@@ -26,8 +34,8 @@ async function Fight() {
   }
 }
 async function Train(){
+  ResetActions()
   Actions.TrainingNow=1;
-  Actions.FightingNow=0;
   while (Actions.TrainingNow==1){
     GameData.powerPerSecond += (GameData.TrainingBoost);
     Display();
