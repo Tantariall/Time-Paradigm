4a810cb2965689bc2bfb7726e20d773b18900fd7
Topottop <burhanovtimur047@gmail.com>
2024-06-28 16:58:25 +0500

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -28,39 +28,41 @@ function Act(x){
    ResetActions()
    if (x==1){
       Actions.TravellingNow=1;
-      Travel();
+      Do().Travel();
    }
    else if (x==2){
       Actions.FightingNow=1;
-      Fight();
+      Do().Fight();
    }
    else if (x==3){
       Actions.TrainingNow=1;
-      Train();
+      Do().Train();
    }
    else{
       //Do Nothing
    }
 };
-async function Fight(){
-   while (Actions.FightingNow==1){
-      Player.Gold += 0.01+(Math.log10(Player.Distance+1));
-      Player.Power += 0.01+(Math.log2(Player.Distance+1))/10;
-      Display();
-      await Delay(10);
+function Do(){
+   async function Fight(){
+      while (Actions.FightingNow==1){
+         Player.Gold += 0.01+(Math.log10(Player.Distance+1));
+         Player.Power += 0.01+(Math.log2(Player.Distance+1))/10;
+         Display();
+         await Delay(10);
+      }
    }
-}
-async function Train(){
-   while (Actions.TrainingNow==1){
-      Player.Power += 1;
-      Display();
-      await Delay(10);
+   async function Train(){
+      while (Actions.TrainingNow==1){
+         Player.Power += 1;
+         Display();
+         await Delay(10);
+      }
    }
-}
-async function Travel(){
-   while (Actions.TravellingNow==1){
-      Player.Distance += ((Player.Power**1.05)-Player.Distance)/500;
-      Display();
-      await Delay(10);
+   async function Travel(){
+      while (Actions.TravellingNow==1){
+         Player.Distance += ((Player.Power**1.05)-Player.Distance)/500;
+         Display();
+         await Delay(10);
+      }
    }
 }
