058fa617b45d70ce1a87ad5a99d6da26ba5bfa3b
Topottop <burhanovtimur047@gmail.com>
2024-06-28 15:00:17 +0500

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 function Delay(time){
    return new Promise(resolve => setTimeout(resolve, time));
-}
+};
 function ResetActions(){
    Actions = {
   FightingNow: 0,
@@ -8,7 +8,7 @@ function ResetActions(){
   TrainingNow: 0,
   MeditatingNow: 0,
    }
-}
+};
 let display = document.getElementById("Stats");
 let Actions = {
   FightingNow: 0,
@@ -23,25 +23,25 @@ let Player = {
 };
 function Display(){
   display.innerHTML =`Your Power is ${Player.Power.toFixed(2)}, Gold ${Player.Gold.toFixed(2)}, Distance ${Player.Distance.toFixed(2)}`;
-}
+};
 function Act(x){
    ResetActions()
    if (x==1){
       Actions.TravellingNow=1;
       //Travel();
-   }
+   };
    else if (x==2){
       Actions.FightingNow=1;
       Fight();
-   }
+   };
    else if (x==3){
       Actions.TrainingNow=1;
       Train();
-   }
+   };
    else{
       //Do Nothing
-   }
-}
+   };
+};
 async function Fight(){
    while (Actions.FightingNow==1){
       Player.Gold += 0.01+(math.log10(Player.Distance));
