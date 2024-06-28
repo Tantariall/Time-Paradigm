81305404f0bb3fc83afcfe86e59b64add9a081fb
Topottop <burhanovtimur047@gmail.com>
2024-06-28 14:49:35 +0500

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-function delay(time){
+function Delay(time){
    return new Promise(resolve => setTimeout(resolve, time));
 }
 function ResetActions(){
@@ -16,31 +16,34 @@ let Actions = {
   TrainingNow: 0,
   MeditatingNow: 0,
 }
-let GameData = {
-  power: 0,
-  powerPerSecond: 1,
-  TrainingBoost: 1,
+let Player = {
+   Power: 0,
+   Gold: 0,
+   Distance: 0,
 };
-async function Act(x){
-   switch (x){
-      case 1:
-         //Travel;
-      case 2:
-         Fight();
-      case 3:
-         Train();
-      case 4:
-         //Meditate;
-   }
-   Display()
-   delay(10)
-}
 function Display(){
   display.innerHTML =`Your Power is ${GameData.power.toFixed(2)}, Fight Efficiency ${GameData.powerPerSecond.toFixed(2)}`;
 }
-function Fight() {
-   GameData.power += (GameData.powerPerSecond/100);
-}
-function Train(){
-   GameData.powerPerSecond += (GameData.TrainingBoost/1000);
-}   
+function Act(x){
+   ResetActions()
+   if (x==1){
+      Actions.TravellingNow=1;
+      //Travel();
+   }
+   else if (x==2){
+      Actions.FightingNow=1;
+      Fight();
+   }
+   else if (x==3){
+      Actions.TrainingNow=1;
+      Train();
+   }
+   else{
+      //Do Nothing
+   }
+async function Fight(){
+   while (Actions.FightingNow==1){
+      Player.Gold += 0.01+(math.log10(Player.Distance));
+      Player.Power += 0.01+(math.log2(Player.Distance));
+      Display();
+      await Delay(10);
